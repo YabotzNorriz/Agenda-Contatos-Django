319d9088edc489f2ab1a9cdbffa72fba7daf5333
João Pedro A.P.P. Barbosa <jpandradepaesdev@outlook.com.br>
2025-03-28 14:29:46 -0300

    "contacts" criada

--- contact/apps.py
+++ contact/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ContatosConfig(AppConfig):
+class ContactConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contatos'
+    name = 'contact'
