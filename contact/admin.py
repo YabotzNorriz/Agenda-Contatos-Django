2832854a2acf96775c780686c0dcd856678fa1bf
João Pedro A.P.P. Barbosa <jpandradepaesdev@outlook.com.br>
2025-03-28 15:22:32 -0300

    Updated admin.py e models.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contact import models
+
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+    )
+    ordering = ("-id",)
